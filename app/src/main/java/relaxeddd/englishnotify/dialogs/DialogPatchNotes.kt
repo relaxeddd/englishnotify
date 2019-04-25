db1678bf970a1973e279bd11b08d09cf469aa25b
Your Name <you@example.com>
2019-04-25 15:45:46 +0300

    1.1.3 remove unusage and new app icon

--- app/src/main/java/relaxeddd/englishnotify/dialogs/DialogPatchNotes.kt
+++ app/src/main/java/relaxeddd/englishnotify/dialogs/DialogPatchNotes.kt
@@ -9,7 +9,7 @@ import relaxeddd.englishnotify.R
 class DialogPatchNotes : DialogFragment() {
 
     companion object {
-        const val VERSION = "1.1.1"
+        const val VERSION = "1.1.3"
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -17,7 +17,7 @@ class DialogPatchNotes : DialogFragment() {
             val builder = AlertDialog.Builder(it)
 
             builder.setTitle(getString(R.string.new_version))
-                .setMessage(getString(R.string.patch_notes_1_1_1))
+                .setMessage(getString(R.string.patch_notes_1_1_3))
                 .setPositiveButton(android.R.string.ok) { _, _ -> }
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
