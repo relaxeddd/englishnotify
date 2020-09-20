8488f03586307858146c5b237774f6cf8cd2fbf3
Chechin Vadim <vadim25091@yandex.ru>
2020-09-20 21:19:11 +0300

    Libraries update, manifest update

--- app/src/main/java/relaxeddd/englishnotify/push/PushTokenHelper.kt
+++ app/src/main/java/relaxeddd/englishnotify/push/PushTokenHelper.kt
@@ -3,8 +3,6 @@ package relaxeddd.englishnotify.push
 import android.app.Activity
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.content.ComponentName
-import android.content.pm.PackageManager
 import android.os.Build
 import relaxeddd.englishnotify.R
 import relaxeddd.englishnotify.common.ENGLISH_WORDS_NOTIFICATIONS_CHANNEL
@@ -12,9 +10,6 @@ import relaxeddd.englishnotify.common.ENGLISH_WORDS_NOTIFICATIONS_CHANNEL
 object PushTokenHelper {
 
     fun initChannelNotifications(activity: Activity) {
-        val componentName = ComponentName(activity, MyFirebaseMessagingService::class.java)
-        activity.packageManager.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP)
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channelId = activity.getString(R.string.default_notification_channel_id)
             val notificationManager = activity.getSystemService(NotificationManager::class.java)
