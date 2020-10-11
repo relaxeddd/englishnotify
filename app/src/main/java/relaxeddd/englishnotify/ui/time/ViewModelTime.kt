df18690be5f382784350aee68c7aa5caafcbdb8d
Chechin Vadim <vadim25091@yandex.ru>
2020-10-11 14:08:31 +0300

    Every 30 minutes option

--- app/src/main/java/relaxeddd/englishnotify/ui/time/ViewModelTime.kt
+++ app/src/main/java/relaxeddd/englishnotify/ui/time/ViewModelTime.kt
@@ -10,7 +10,7 @@ import relaxeddd.englishnotify.model.repository.RepositoryUser
 class ViewModelTime(private val repositoryUser: RepositoryUser) : ViewModelBase() {
 
     val isDisableSubTime: Boolean = (repositoryUser.liveDataUser.value?.subscriptionTime ?: 0) <= System.currentTimeMillis()
-    var receiveNotificationsTime: Int = repositoryUser.liveDataUser.value?.notificationsTimeType ?: 3
+    var receiveNotificationsTime: Int = repositoryUser.liveDataUser.value?.notificationsTimeType ?: 7
 
     val checkedChangeListenerTime = RadioGroup.OnCheckedChangeListener { view, _ ->
         val radioButton = view.findViewById<RadioButton>(view?.checkedRadioButtonId ?: 0)
@@ -20,7 +20,7 @@ class ViewModelTime(private val repositoryUser: RepositoryUser) : ViewModelBase(
     fun onClickAccept() {
         val user = repositoryUser.liveDataUser.value ?: return
 
-        if (receiveNotificationsTime < 3 && (user.subscriptionTime) < System.currentTimeMillis()) {
+        if (receiveNotificationsTime < 7 && (user.subscriptionTime) < System.currentTimeMillis()) {
             showToast(R.string.subscription_need)
         } else {
             uiScope.launch {
